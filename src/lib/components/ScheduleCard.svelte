68cf6381c7d3ebcb7762b1d49c6612d44fd16239
Alex <alex@codingcat.dev>
2023-04-03 19:42:37 -0400

    change schedule time location

--- src/lib/components/ScheduleCard.svelte
+++ src/lib/components/ScheduleCard.svelte
@@ -9,16 +9,18 @@
 	$: toDate = `${new Date(toTime).toLocaleTimeString()}`;
 </script>
 
-<div class="bcu-card variant-ghost-surface">
-	<a class="!no-underline !text-token" href={`/schedule/${slug}`}>
-		<header class="bcu-card-header">
-			<h3 class="text-xl lg:text-2xl xl:text-3xl">{title}</h3>
-		</header>
-		<section class="p-4">
-			<p>{desc}</p>
-		</section>
-		<footer class="bcu-card-footer">
-			<p class="text-xs">{`${fromDate} - ${toDate}`}</p>
-		</footer>
-	</a>
+<div class="flex flex-col">
+	<div class="pl-2 mb-4">
+		<span class="text-xl text-primary-500">{`${fromDate} - ${toDate}`}</span>
+	</div>
+	<div class="bcu-card variant-ghost-surface">
+		<a class="!no-underline !text-token" href={`/schedule/${slug}`}>
+			<header class="bcu-card-header">
+				<h3 class="text-xl lg:text-2xl xl:text-3xl">{title}</h3>
+			</header>
+			<section class="p-4 opacity-60">
+				<p>{desc}</p>
+			</section>
+		</a>
+	</div>
 </div>
