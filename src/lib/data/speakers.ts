59c6128852eb079c9e1b90f2a4794add5f71c2ef
Alex Patterson <alex@codingcat.dev>
2023-04-05 12:12:16 -0400

    Update speakers.ts

--- src/lib/data/speakers.ts
+++ src/lib/data/speakers.ts
@@ -7,14 +7,5 @@ export const speakers = [
 			'https://media.codingcat.dev/image/upload/main-codingcatdev-photo/headshots/alex_headshot_1.jpg',
 		bio:
 			'Alex is the founder of CodingCatDev, where they create “Purrfect Web Tutorials” to teach the world how to turn their development dreams into reality. Alex is a professional full stack developer, and he is passionate about mentoring new developers and helping the community that has allowed him to live his development dreams. Alex firmly believes that anyone can learn to be a developer. The CodingCatDev team is here to help!'
-	},
-	{
-		slug: 'nandeesh-tankasala',
-		name: 'Nandeesh Tankasala',
-		title: 'Software Engineer',
-		pic:
-			'https://ca.slack-edge.com/T02MRNE0U-U022QB9RRPC-b3c3002a1571-512',
-		bio:
-			'Getting busy in crafting mobile solutions to solve real world problems.'
 	}
 ];
