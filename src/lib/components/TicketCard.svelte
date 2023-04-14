3efc325452353fef0135717d62db9f9492e7980f
Matt Gehrls <mattgehrls@gmail.com>
2023-04-14 13:32:17 -0400

    mostly added spacing, and max widths

--- src/lib/components/TicketCard.svelte
+++ src/lib/components/TicketCard.svelte
@@ -1,9 +1,9 @@
-<div class="w-full bcu-card variant-ringed-primary">
-	<header class="bcu-card-header">
-		<h2 class="text-xl lg:text-2xl xl:text-3xl">Your Ticket Includes</h2>
+<div class="w-full bcu-card variant-ringed-primary p-4">
+	<header class="bcu-card-header mt-4">
+		<h2 class="text-xl lg:text-2xl xl:text-3xl font-bold text-center">Your Ticket Includes</h2>
 	</header>
-	<section class="p-4">
-		<ul class="flex flex-col gap-8 text-sm lg:text-lg xl:text-xl">
+	<section class="p-4 my-8">
+		<ul class="flex flex-col text-center gap-4 text-sm lg:text-lg xl:text-xl">
 			<li>🎤 Full day of expert speaker sessions online and in person</li>
 			<li>
 				💃 Afterparty at <a href="https://www.newhollandbrew.com/location/grand-rapids/">
@@ -14,9 +14,9 @@
 			<li>✨ An unforgettable time with the Developer community in Grand Rapids, MI</li>
 		</ul>
 	</section>
-	<footer class="bcu-card-footer">
+	<footer class="bcu-card-footer flex justify-center mb-4">
 		<a
-			class="w-full bcu-button variant-filled-primary"
+			class="w-3/4 bcu-button variant-filled-primary"
 			href="https://www.eventbrite.com/e/google-io-extended-grand-rapids-2023-tickets-605068284527"
 			target="_blank"
 			rel="noopener noreferrer"
--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -7,31 +7,36 @@
 </script>
 
 <LayoutWrapper>
-	<div class="flex gap-2 md:gap-8">
-		<h1>Schedule for Google I/O Extended - Grand Rapids</h1>
-		<p class="bcu-badge variant-filled-primary">May 10th, 2023</p>
-	</div>
-	<div class="bcu-alert variant-filled-secondary text-lg">
-		This agenda is not final! All times, events, and speakers may (and most likely will) change.
-	</div>
-	<div class="w-full bcu-card">
-		<header class="bcu-card-header">
-			<h2 class="text-xl lg:text-2xl xl:text-3xl">May 10th, 2023</h2>
-		</header>
+	<div class="flex m-2 md:m-8 justify-center">
+		<div class="flex flex-col gap-2 md:gap-12 max-w-6xl">
+			<div class="flex gap-2 md:gap-8">
+				<h1>Schedule for Google I/O Extended - Grand Rapids</h1>
+				<p class="bcu-badge variant-filled-primary">May 10th, 2023</p>
+			</div>
+			<div class="bcu-alert variant-filled-secondary text-lg">
+				⚠ This agenda is not final! All times, events, and speakers may (and most likely will)
+				change. ⚠
+			</div>
+			<div class="w-full bcu-card">
+				<header class="bcu-card-header">
+					<h2 class="text-xl lg:text-2xl xl:text-3xl">May 10th, 2023</h2>
+				</header>
 
-		<!-- Events -->
-		<section
-			class="grid grid-cols-1 lg:grid-cols-[1fr_1fr] gap-8 px-8 my-8 overflow-y-auto md:gap-16 md:my-16"
-		>
-			{#each data.schedules as schedule}
-				<ScheduleCard
-					title={schedule.title}
-					summary={schedule.summary}
-					fromTime={schedule.fromTime}
-					toTime={schedule.toTime}
-					slug={schedule.slug}
-				/>
-			{/each}
-		</section>
+				<!-- Events -->
+				<section
+					class="grid grid-cols-1 lg:grid-cols-[1fr_1fr] gap-8 px-8 my-8 overflow-y-auto md:gap-16 md:my-16"
+				>
+					{#each data.schedules as schedule}
+						<ScheduleCard
+							title={schedule.title}
+							summary={schedule.summary}
+							fromTime={schedule.fromTime}
+							toTime={schedule.toTime}
+							slug={schedule.slug}
+						/>
+					{/each}
+				</section>
+			</div>
+		</div>
 	</div>
 </LayoutWrapper>
