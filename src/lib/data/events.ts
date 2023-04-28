86dbae224f0185ee98d6b9cbbfc51bb39a8426ea
Alex <alex@codingcat.dev>
2023-04-28 16:02:24 -0400

    add speakers

--- src/lib/data/events.ts
+++ src/lib/data/events.ts
@@ -1,4 +1,22 @@
 export const events = [
+	// {
+	// 	title: 'Svelte Kit and Firebase: The Perfect Combo for Rapid Development',
+	// 	summary:
+	// 		'In this workshop, we will show how to use Svelte Kit and Firebase to build CodingCat.dev using BlackCatUI.',
+	// 	body: `Svelte Kit and Firebase are two of the most popular tools for building modern web applications. Svelte Kit is a framework that makes it easy to build web applications with Svelte, while Firebase is a cloud-based backend as a service that provides a number of features, including authentication, storage, and databases.
+
+	//     In this workshop, we will show how to use Svelte Kit and Firebase to build CodingCat.dev using BlackCatUI. We will cover the following topics:
+
+	//     Setting up Svelte Kit and Firebase
+	//     Authenticating users with Svelte Kit
+	//     Storing user data in Firestore
+
+	//     By the end of this workshop, you will be able to build a simple web application with authentication using Svelte Kit and Firebase. `,
+	// 	fromTime: new Date('May 10, 2023 15:00').getTime(),
+	// 	toTime: new Date('May 10, 2023 15:45').getTime(),
+	// 	slug: 'svelte-kit-and-firebase',
+	// 	authorSlug: 'alex-patterson'
+	// },
 	{
 		title: 'Scrum is not a Silver Bullet',
 		summary:
@@ -22,20 +40,30 @@ export const events = [
 		authorSlug: 'abu-anwar-md-abdullah'
 	},
 	{
+		title: 'Lightning Fast Webapps on the Serverless Cloud',
+		summary: 'Building high performance webapps is easier than ever with the serverless cloud. We’ll discuss how you can rapidly, efficiently, and cheaply deploy Server Side Rendered (SSR) apps on Google Cloud Run and Firebase.',
+		body: `Building high performance webapps is easier than ever with the serverless cloud. We’ll discuss how you can rapidly, efficiently, and cheaply deploy Server Side Rendered (SSR) apps on Google Cloud Run and Firebase. We’ll work through building a modern site with live examples and cover the pros and cons of this approach, showing that serverless backends are an ideal match for webapp frontends.
+		`,
+		fromTime: new Date('May 10, 2023 12:00').getTime(),
+		toTime: new Date('May 10, 2023 12:45').getTime(),
+		slug: 'lightning-fast-webapps-on-the-serverless-cloud',
+		authorSlug: 'joe-lust'
+	},
+	{
 		title: 'Keynote',
 		summary: `Remote Keynote from Google`,
 		body: `Tune in to find out how we're furthering our mission to organize the world's information and make it universally accessible and useful.`,
-		fromTime: new Date('May 10, 2023 12:00').getTime(),
-		toTime: new Date('May 10, 2023 13:00').getTime(),
+		fromTime: new Date('May 10, 2023 13:00').getTime(),
+		toTime: new Date('May 10, 2023 14:00').getTime(),
 		slug: 'keynote',
 		authorSlug: 'sundar-pichai'
 	},
 	{
-		title: 'Lunch / Keynote',
+		title: 'Lunch / Developer Keynote',
 		summary: `Food from New Holland Knickerbocker`,
 		body: `Continue watching Keynote while enjoying each others company.`,
-		fromTime: new Date('May 10, 2023 13:00').getTime(),
-		toTime: new Date('May 10, 2023 14:00').getTime(),
+		fromTime: new Date('May 10, 2023 14:00').getTime(),
+		toTime: new Date('May 10, 2023 14:30').getTime(),
 		slug: 'lunch',
 		authorSlug: 'sundar-pichai'
 	},
@@ -53,28 +81,43 @@ export const events = [
 		By the end of this talk, you will have a clear understanding of how to implement a monorepo workflow for your design system using PNPM workspaces, NX, and SvelteKit. You will be able to streamline your development process, improve code consistency, and make it easier to manage your design system components.
 		
 		`,
-		fromTime: new Date('May 10, 2023 14:00').getTime(),
-		toTime: new Date('May 10, 2023 14:45').getTime(),
+		fromTime: new Date('May 10, 2023 14:30').getTime(),
+		toTime: new Date('May 10, 2023 15:15').getTime(),
 		slug: 'a-monorepo-workflow-for-your-design-system',
 		authorSlug: 'brittney-postma'
 	},
 	{
-		title: 'Svelte Kit and Firebase: The Perfect Combo for Rapid Development',
-		summary:
-			'In this workshop, we will show how to use Svelte Kit and Firebase to build CodingCat.dev using BlackCatUI.',
-		body: `Svelte Kit and Firebase are two of the most popular tools for building modern web applications. Svelte Kit is a framework that makes it easy to build web applications with Svelte, while Firebase is a cloud-based backend as a service that provides a number of features, including authentication, storage, and databases.
+		title: 'A Monorepo Workflow for your Design System',
+		summary: 'In this talk, we will explore how to implement a monorepo workflow for your design system using PNPM workspaces, NX, and SvelteKit.',
+		body: `In this talk, we will explore how to implement a monorepo workflow for your design system using PNPM workspaces, NX, and SvelteKit. By consolidating all your packages and sites into a single codebase, you can streamline your development, packaging, and publishing process.
 
-        In this workshop, we will show how to use Svelte Kit and Firebase to build CodingCat.dev using BlackCatUI. We will cover the following topics:
-        
-        Setting up Svelte Kit and Firebase
-        Authenticating users with Svelte Kit
-        Storing user data in Firestore
-        
-        By the end of this workshop, you will be able to build a simple web application with authentication using Svelte Kit and Firebase. `,
-		fromTime: new Date('May 10, 2023 15:00').getTime(),
-		toTime: new Date('May 10, 2023 15:45').getTime(),
-		slug: 'svelte-kit-and-firebase',
-		authorSlug: 'alex-patterson'
+		We will start by introducing the concept of monorepos and their benefits, before diving into the specific tools and frameworks that make it possible. We will explore PNPM workspaces, a lightweight package manager that allows you to manage all your dependencies within a single repository.
+		
+		Next, we will discuss NX, a powerful tool for building monorepo-based projects. NX provides a suite of tools for managing your monorepo, including code generators, testing tools, and much more. We will explore how to use NX to set up your monorepo and manage your design system components.
+		
+		Finally, we will discuss SvelteKit, a framework for building web applications that is optimized for monorepo workflows. We will explore how to use SvelteKit to build out your design system components and how it can easily package up your components for publishing to NPM.
+		
+		By the end of this talk, you will have a clear understanding of how to implement a monorepo workflow for your design system using PNPM workspaces, NX, and SvelteKit. You will be able to streamline your development process, improve code consistency, and make it easier to manage your design system components.
+		
+		`,
+		fromTime: new Date('May 10, 2023 14:30').getTime(),
+		toTime: new Date('May 10, 2023 15:15').getTime(),
+		slug: 'a-monorepo-workflow-for-your-design-system',
+		authorSlug: 'brittney-postma'
+	},
+	{
+		title: 'Forces Shaping AI You Might Not Realize',
+		summary: 'Learn about many of the drivers changing the landscape of AI, from an academically trained futurist and engineering leader.',
+		body: `Learn about many of the drivers changing the landscape of AI, from an academically trained futurist and engineering leader. We all want to know what comes next, and we can learn a lot from looking across the broader world about the possibilities. I'll share details from my research into these forces, including some unexpected ones.
+
+		You might work in AI, or be interested in understanding how AI will impact you. Armed with this information, you can ensure your strategies aren't missing some key details. By being aware of trends you can help shape your part of the world towards a preferred future.
+		
+		I'll also cover a bit about how to incorporate strategic foresight into your practices so you can leverage uncertainty in a positive way to anticipate change.
+		`,
+		fromTime: new Date('May 10, 2023 15:15').getTime(),
+		toTime: new Date('May 10, 2023 16:00').getTime(),
+		slug: 'forces-shaping-ai-you-might-not-realize',
+		authorSlug: 'jeremy-wilken'
 	},
 	{
 		title: 'The Velocity Trap',
@@ -84,5 +127,25 @@ export const events = [
 		toTime: new Date('May 10, 2023 16:45').getTime(),
 		slug: 'the-velocity-trap',
 		authorSlug: 'brad-nelson'
+	},
+	{
+		title: 'Developing with a Design Mindset',
+		summary: `As Artificial Intelligence accelerates the code-writing process, our role as developers needs to expand beyond functions and breakpoints.
+		`,
+		body: `
+		As Artificial Intelligence accelerates the code-writing process, our role as developers needs to expand beyond functions and breakpoints. By thinking about the human element, and writing code with the user experience in mind, developers can become an integral part of the design process.
+
+		Recently, in my twitter feed, John Carmack (an engineering legend in the area of 3D graphics) was asked about developers and AI. Specifically, a younger developer was asking about career opportunities for system engineers when Artificial Intelligence is already being used to write code. His reply was this…
+
+		"Build full 'product skills' and use the best tools for the job…Software is just a tool to accomplish something for people. Keep your eyes on delivered value, and don't over-focus on the specifics of the tools."
+		
+		"Building for people" I think this is something important that modern developers need to hear. While it feels good to solve complex problems or create elegant systems, it's easy to fall in love with the coding side of development and forget about the people side.
+		
+		In my years working as an artist and developer in the game industry, there are several tricks and methods I've used to ensure the work being done was always in service of a better product. This talk will help developers take a step back from coding to look at their project holistically, encouraging them to become a vital part of the creative process.
+		`,
+		fromTime: new Date('May 10, 2023 17:00').getTime(),
+		toTime: new Date('May 10, 2023 17:45').getTime(),
+		slug: 'developing-with-a-design-mindset',
+		authorSlug: 'scott-tykoski'
 	},
 ];
--- src/lib/data/speakers.ts
+++ src/lib/data/speakers.ts
@@ -1,13 +1,13 @@
 export const speakers = [
-	{
-		slug: 'alex-patterson',
-		name: 'Alex Patterson',
-		title: 'Developer Advocate',
-		pic:
-			'https://media.codingcat.dev/image/upload/main-codingcatdev-photo/headshots/alex_headshot_1.jpg',
-		bio:
-			'Alex is the founder of CodingCatDev, where they create “Purrfect Web Tutorials” to teach the world how to turn their development dreams into reality. Alex is a professional full stack developer, and he is passionate about mentoring new developers and helping the community that has allowed him to live his development dreams. Alex firmly believes that anyone can learn to be a developer. The CodingCatDev team is here to help!'
-	},
+	// {
+	// 	slug: 'alex-patterson',
+	// 	name: 'Alex Patterson',
+	// 	title: 'Developer Advocate',
+	// 	pic:
+	// 		'https://media.codingcat.dev/image/upload/main-codingcatdev-photo/headshots/alex_headshot_1.jpg',
+	// 	bio:
+	// 		'Alex is the founder of CodingCatDev, where they create “Purrfect Web Tutorials” to teach the world how to turn their development dreams into reality. Alex is a professional full stack developer, and he is passionate about mentoring new developers and helping the community that has allowed him to live his development dreams. Alex firmly believes that anyone can learn to be a developer. The CodingCatDev team is here to help!'
+	// },
 	{
 		slug: 'abu-anwar-md-abdullah',
 		name: 'Abu Anwar MD Abdullah',
@@ -53,6 +53,33 @@ export const speakers = [
 		pic:
 			'https://sessionize.com/image/22c9-200o200o2-E8mARoxD3GMhcqyB2B2hPb.png',
 		bio: `Brad Nelson is an industry-recognized product strategy and agility professional within Insight Consulting Services. His range of expertise brings people together to enhance Insight's capabilities as a Solutions Integrator that partners with organizations to achieve their business goals. Brad has repeatedly demonstrated his ability to grow high-performing teams across domains and industries and has instructed countless people from kids to seasoned professionals. He is a creator of content marketing and offerings for Insight in Agile, DevOps, and Product. He co-hosts The Agile For Agilists Podcast, is an experienced public speaker, and has instructed countless people from children to seasoned professionals.
+		`
+	},
+	{
+		slug: 'joe-lust',
+		name: 'Joe Lust',
+		title: 'Google Developer Expert in Cloud @ mabl',
+		pic:
+			'https://sessionize.com/image/f8d1-200o200o2-Sv7fTQAjreyrLBqg4g5Ksi.png',
+		bio: `Joe is a passionate lecturer, blogger, organizer, and software engineer with a focus on creating distributed cloud systems and architectures. He is a maker, crafting cloud systems @ mabl professionally as the cloud infrastructure lead, and spending his nights and weekends crafting tech content and co-organizing the Boston GDG Cloud chapter. He also is the lead engineer and co-founder at medical analytics firm MDDetails. Joe has spoken at conferences like Google Cloud Next, GitHub Satellite, GitLab Commit, Atlassian Team, and STPCon. He is a recognized engineer, as a Google Developer Expert in cloud, Google Cloud Innovator Champion, and GitHub Champion. In the analog world, he is a dedicated husband, father, distance runner, and amateur carpenter.
+		`
+	},
+	{
+		slug: 'jeremy-wilken',
+		name: 'Jeremy Wilken',
+		title: 'Engineering Leadership & Futurist',
+		pic:
+			'https://sessionize.com/image/a38a-200o200o2-cc-1c0a-4167-bd31-9dc4ddc06dfb.ed39780d-d4a9-4e87-9ea7-f364062df14a.jpg',
+		bio: `Jeremy is strategic thinker and software engineering leader focusing on how people and technology intersect. His key area of focus is AI and the trends that are challenging its growth or reinforcing its value. He uses strategic foresight to anticipate trends and leads efforts to co-create positive futures. He has authored two books, spoken globally at events, run podcasts, and is recognized by Google as a Developer Expert. He lives with his family in Austin TX.
+		`
+	},
+	{
+		slug: 'scott-tykoski',
+		name: 'Scott Tykoski',
+		title: 'Sr. Development Consultant at OST',
+		pic:
+			'https://sessionize.com/image/5394-200o200o2-ncGZi6jjADzyL3ALR9JbxX.png',
+		bio: `Scott is a developer, designer, and consultant with over 20 years of tech industry experience. He is always looking for new ways to leverage design methodologies with emerging technology, like the Unity engine, which he wrote extensively about in 'Mastering Game Design with Unity 2021'.
 		`
 	},
 ];
